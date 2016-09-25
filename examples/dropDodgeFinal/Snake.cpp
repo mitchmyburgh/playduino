9b88153ec99d0218fb10d8d07dfaa141da5a86e3
Mitch Myburgh <mitchmyburgh@gmail.com>
2016-09-25 11:10:14 +0200

    updated classes for dropDodgeFinal

--- examples/dropDodgeFinal/Snake.cpp
+++ examples/dropDodgeFinal/Snake.cpp
@@ -15,13 +15,15 @@
  */
 Snake::Snake(int x_, int y_, int screenWidth_, int screenHeight_) {
   node = new Entity(x_, y_, screenWidth_, screenHeight_);
+  //hold the previous move for propogation down the snake
   moveX = 0;
   moveY = 0;
   next = NULL;
 }
 
 /**
- * Move the entity with screen wrap around
+ * Add node to the snake
+ * Adds teh node to the end of the snake and at the same position as the last node
  */
 void Snake::addNode() {
   Snake* currentNode = this;
@@ -31,60 +33,84 @@ void Snake::addNode() {
   currentNode->addNext();
 }
 
+/**
+ * Get teh ndoe for the current snake part
+ */
 Entity* Snake::getNode() {
   return node;
 }
 
+/**
+ * Get the next snake part
+ */
 Snake* Snake::getNext() {
   return next;
 }
 
+/**
+ * Add a node after the current one (in the same position)
+ */
 void Snake::addNext() {
   next = new Snake(node->getX(), node->getY());
+  next->setCollidable(false); //the node is currently under another node so dont make it collidable
 }
 
+/**
+ * Move the snake
+ * int x_ - the number of pixels in the x-direction to move
+ * int y_ - the number of pixels in the y direction to move
+ */
 void Snake::move(int x_, int y_){
-  if (x_ == 0 && y_ == 0){
+  if (x_ == 0 && y_ == 0){ //continue moving in the same direction
     x_ = moveX;
     y_ = moveY;
   } else {
-    collidable = true;
+    collidable = true;  //the last pixel becomes collidable when it is no longer covered by a pixel
   }
   if (moveX != 0 || moveY != 0){
     node->move(x_, y_);
   }
   if (this->getNext() != NULL){
-    this->getNext()->move(moveX, moveY);
+    this->getNext()->move(moveX, moveY); //propogate movement down the linked list
   }
-  moveX = x_;
+  //store current movement for next loop
+  moveX = x_; 
   moveY = y_;
 }
 
+/**
+ * Check collision of the snake with itsself
+ */
 bool Snake::checkCollision() {
   int prevX = this->getNode()->getX();
   int prevY = this->getNode()->getY();
   Snake* currentNode = this->getNext();
-  do{
+  while (currentNode != NULL) { //loop over nodes
      if (currentNode->getNode()->checkCollision(prevX, prevY) && currentNode->getCollidable()){
        return true;
      }
      currentNode = currentNode->getNext();
-  } while (currentNode != NULL);
+  }
   return false;
 }
 
+/**
+ * Get if the current node is able to collide
+ */
 bool Snake::getCollidable(){
   return collidable;
 }
 
+/**
+ * Set if the current node is able to collide
+ */
 void Snake::setCollidable(bool col){
   collidable = col;
 }
-
+ /*
+  * Destructor
+  */
 Snake::~Snake(){
   delete node;
   delete next; // delete single object pointer
-
-  // delete[] for arrays of objects
-
 }
--- examples/dropDodgeFinal/Snake.h
+++ examples/dropDodgeFinal/Snake.h
@@ -10,17 +10,13 @@
 class Snake
 {
   private:
-      Entity* node;
-      Snake* next;
-      int moveX = 0;
-      int moveY = 0;
-      bool collidable = false;
+      Entity* node; // each snake has an entity as a node
+      Snake* next; // the next snake in the linked list
+      int moveX = 0; // the previous x move from propogating down the snake 
+      int moveY = 0; // the previous y move for propogation down the snake
+      bool collidable = false; // is the current snake allowed to collide with other snakes
    public:
       Snake(int x_ = 0, int y_ = 0, int screenWidth_ = 8, int screenHeight_ = 8);
-
-      // destructor : destroy objects at end of runtime
-      ~Snake();
-
       void move(int moveX = 0, int moveY = 0);
       void addNode();
       Snake* getNext();
@@ -29,6 +25,7 @@ class Snake
       bool checkCollision();
       void setCollidable(bool col);
       bool getCollidable();
-      //bool checkCollision(int checkX, int checkY);
+      // destructor : destroy objects at end of runtime
+      ~Snake();
 };
 #endif
